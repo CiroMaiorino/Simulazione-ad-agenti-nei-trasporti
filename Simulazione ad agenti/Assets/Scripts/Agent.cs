f5cbc94708f00f2a379ec093a2d268735ac4413e
Tekhorror <ciromaiorino99@gmail.com>
2021-05-11 17:13:37 +0200

    Gli agenti si siedono correttamente
    Bisogna annullare il pathing dopo che si sono seduti, per permettere il movimento del bus
    
    Co-Authored-By: Montefusco Antonio Renato <64643946+AntonioRenatoMontefusco@users.noreply.github.com>

--- Simulazione ad agenti/Assets/Scripts/Agent.cs
+++ Simulazione ad agenti/Assets/Scripts/Agent.cs
@@ -8,10 +8,17 @@ public class Agent : MonoBehaviour
     // Start is called before the first frame update
     AIDestinationSetter destinationSetter;
     public Pullman pullman;
-  
+    private Animator animator;
+    private IAstarAI ai;
+    private GameObject passengers;
+
+    public float mammt=0f;
     void Start()
     {
+        passengers = pullman.transform.Find("Passengers").gameObject;
         destinationSetter = GetComponent<AIDestinationSetter>();
+        animator = this.GetComponentInChildren<Animator>();
+        ai = GetComponent<IAstarAI>();
 
         if (!pullman.isMooving && pullman.freeTarget() != null)
             setDestination(pullman.freeTarget());
@@ -20,12 +27,21 @@ public class Agent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
+        if(ai.reachedDestination){
+            Debug.Log(ai.rotation);
+            animator.SetBool("Waiting",true);
+            transform.rotation =Quaternion.identity;
+            animator.SetBool("Sit",true);
+            transform.parent=passengers.transform;
+            Destroy(GetComponentInChildren<Rigidbody>());
+            
+        }
+        
         }
 
     public void setDestination(Target t){
         destinationSetter.target=t.transform;
         t.isOccupied=true;
     }
+    
 }
