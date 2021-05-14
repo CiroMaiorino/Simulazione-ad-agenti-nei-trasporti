8735f03433ead38bb11a5e798827dc5c926d253c
Tekhorror <ciromaiorino99@gmail.com>
2021-05-14 20:12:50 +0200

    Spawning di agent tramite numero
    Gli agent spawnano tutti quando si preme il tasto P.
    Da risolvere: Gli agent non camminano allo spawn; Missing Behaviour.
    
    Co-Authored-By: Montefusco Antonio Renato <64643946+AntonioRenatoMontefusco@users.noreply.github.com>

---
+++ Simulazione ad agenti/Assets/Scripts/GameControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class GameControl : MonoBehaviour
+{
+
+    public GameObject agentPrefab;
+    public Transform Spawnpoint;
+    private Bus bus;
+    private Agent agent;
+    public int numeroAgenti;
+
+    void Start() {
+        
+    }
+
+     void Update(){
+            
+            if(Input.GetKeyDown(KeyCode.P)){
+
+                for(;numeroAgenti>0;numeroAgenti--){
+                    spawnAgent();
+                }
+            }
+    }
+    public void spawnAgent(){
+        
+        var position =new Vector3(Random.Range(-5.0f, 5.0f),0,Random.Range(-5.0f,5.0f));
+
+        GameObject gameOb=Instantiate(agentPrefab,position,Spawnpoint.rotation);
+        bus= GameObject.Find("BasicBus").GetComponent<Bus>();
+        agent=gameOb.GetComponent<Agent>();
+        agent.bus=bus;
+        
+        
+        
+    }
+    
+    
+}
