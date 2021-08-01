75f8fa6fd92bf384b477abf53d6c4b712744dde1
Ciro Maiorino <ciromaiorino99@gmail.com>
2021-08-01 16:24:23 +0200

    Added Clock
    Co-Authored-By: Montefusco Antonio Renato <64643946+AntonioRenatoMontefusco@users.noreply.github.com>

---
+++ Simulazione ad agenti/Assets/Scripts/ClockUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockUI : MonoBehaviour {
+
+    private const float REAL_SECONDS_PER_INGAME_DAY = 17280f;
+
+    private Transform clockHourHandTransform;
+    private Transform clockMinuteHandTransform;
+    private Text timeText;
+    private float day;
+
+    private void Awake() {
+        clockHourHandTransform = transform.Find("hourHand");
+        clockMinuteHandTransform = transform.Find("minuteHand");
+        timeText = transform.Find("timeText").GetComponent<Text>();
+    }
+
+    private void Update() {
+        day += Time.deltaTime / REAL_SECONDS_PER_INGAME_DAY;
+
+        float dayNormalized = day % 1f;
+
+        float rotationDegreesPerDay = 720f;
+        clockHourHandTransform.eulerAngles = new Vector3(0, 0, -dayNormalized * rotationDegreesPerDay);
+
+        float hoursPerDay = 24f;
+        clockMinuteHandTransform.eulerAngles = new Vector3(0, 0, -dayNormalized * rotationDegreesPerDay * hoursPerDay);
+
+        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
+
+        float minutesPerHour = 30f;
+        string minutesString = Mathf.Floor(((dayNormalized * hoursPerDay) % 1f) * minutesPerHour).ToString("00");
+
+        timeText.text = hoursString + ":" + minutesString;
+    }
+
+}
